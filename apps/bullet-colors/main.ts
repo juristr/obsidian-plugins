39232e711dbdac00559e026979ed45ce92c104ef
Juri <juri.strumpflohner@gmail.com>
2023-05-21 09:27:42 +0200

    adding colors plugin

---
+++ apps/bullet-colors/main.ts
@@ -0,0 +1,55 @@
+import { MarkdownPostProcessorContext, Plugin } from "obsidian";
+
+// Remember to rename these classes and interfaces!
+
+// interface MyPluginSettings {
+// 	mySetting: string;
+// }
+
+// const DEFAULT_SETTINGS: MyPluginSettings = {
+// 	mySetting: 'default'
+// }
+
+export default class BulletColoringPlugin extends Plugin {
+	async onload() {
+		// obsidian.MarkdownPreviewRenderer.registerPostProcessor(
+		//   this.tagColorProcessor
+		// );
+
+		// this.registerCodeMirror((cm) => {
+		//   cm.on('change', () => {
+		//     // Get the currently active line
+		//     const line = cm.getCursor().line;
+
+		//     // Get the DOM element representing the line
+		//     const lineElement = cm.getLineHandle(line);
+
+		//     console.log('bullet', line, lineElement);
+
+		//     this.tagColorProcessor(lineElement, null);
+		//   });
+		// });
+
+		this.registerMarkdownPostProcessor(this.tagColorProcessor);
+	}
+
+	onunload() {}
+
+	tagColorProcessor = (
+		element: HTMLElement,
+		ctx: MarkdownPostProcessorContext
+	) => {
+		const liBullets = element.querySelectorAll("li");
+		if (!liBullets.length) return;
+
+		liBullets.forEach((li) => {
+			if (li.innerText.startsWith("I:")) {
+				li.addClass("bullet-task-important");
+			} else if (li.innerText.startsWith("T:")) {
+				li.addClass("bullet-task-open");
+			} else if (li.innerText.startsWith("D:")) {
+				li.addClass("bullet-task-done");
+			}
+		});
+	};
+}
